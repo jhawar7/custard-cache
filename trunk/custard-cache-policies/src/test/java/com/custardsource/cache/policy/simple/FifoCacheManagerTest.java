70ccd16371d05f8d161cf980a19b0f8ac4351578
funkwit <funkwit@2972ba69-cd50-0410-870e-89e834fb94c0>
2008-07-08 12:21:05 +0000

    Pulled out parent class

--- trunk/custard-cache-policies/src/test/java/com/custardsource/cache/policy/simple/FifoCacheManagerTest.java
+++ trunk/custard-cache-policies/src/test/java/com/custardsource/cache/policy/simple/FifoCacheManagerTest.java
@@ -1,10 +1,10 @@
 package com.custardsource.cache.policy.simple;
 
-import com.custardsource.cache.policy.CacheManagerListener;
+import com.custardsource.cache.policy.BasicCacheStateTest;
 
-import junit.framework.TestCase;
 
-public class FifoCacheManagerTest extends TestCase {
+
+public class FifoCacheManagerTest extends BasicCacheStateTest {
 	public void testQueueState() {
 		FifoCacheManager<Character> manager = new FifoCacheManager<Character>(
 				new FifoConfiguration(3));
@@ -15,37 +15,5 @@ public class FifoCacheManagerTest extends TestCase {
 		assertStateAfterVisit(manager, 'C', "[A, B, C]");
 		assertStateAfterVisit(manager, 'D', "[B, C, D]", listener, 1);
 		assertStateAfterVisit(manager, 'C', "[B, C, D]");
-	}
-
-	private void assertStateAfterVisit(FifoCacheManager<Character> manager,
-			char visit, String expectedState) {
-		manager.visit(visit);
-		assertEquals("Cache state does not match expected", expectedState,
-				manager.debugString());
-	}
-
-	private void assertStateAfterVisit(FifoCacheManager<Character> manager,
-			char visit, String expectedState, EvictionListener listener,
-			int expectedEvictions) {
-		assertStateAfterVisit(manager, visit, expectedState);
-		assertEquals("Expected eviction count does not match",
-				expectedEvictions, listener.getAndReset());
-	}
-
-	public class EvictionListener implements CacheManagerListener<Character> {
-		private int evictionCount = 0;
-
-		public void objectReadyForEviction(Character item) {
-			evictionCount++;
-		}
-
-		public int getAndReset() {
-			int result = evictionCount;
-			evictionCount = 0;
-			return result;
-		}
-
-		public void objectLoaded(Character item) {
-		}
 	}
 }
