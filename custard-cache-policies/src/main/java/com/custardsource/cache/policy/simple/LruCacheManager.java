786d46e2cd530687cd885f2da41f98d5addbeac7
funkwit <funkwit@2972ba69-cd50-0410-870e-89e834fb94c0>
2008-07-08 12:47:16 +0000

    Implementation of LRU cache

---
+++ custard-cache-policies/src/main/java/com/custardsource/cache/policy/simple/LruCacheManager.java
@@ -0,0 +1,50 @@
+package com.custardsource.cache.policy.simple;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Queue;
+
+import com.custardsource.cache.policy.BaseCacheManager;
+import com.custardsource.cache.policy.QueueAdapter;
+
+public class LruCacheManager<T> extends BaseCacheManager<T> {
+    private final Queue<T> queue = new QueueAdapter<T>(new LinkedHashSet<T>());
+    private final LruConfiguration config;
+    
+    public LruCacheManager(LruConfiguration config) {
+        this.config = config;
+    }
+    
+    public void add(T hit) {
+        // TODO?
+    }
+
+    public int cacheSize() {
+        return queue.size();
+    }
+
+    public void clear() {
+        queue.clear();
+    }
+
+    public void remove(T hit) {
+        queue.remove(hit);
+    }
+
+    public void visit(T hit) {
+    	boolean existed = queue.remove(hit);
+    	queue.add(hit);
+        
+    	if (!existed) {
+            load(hit);
+            if (queue.size() > config.getMaxNodes()) {
+                evict(queue.remove());
+            }
+    	}
+    }
+
+	@Override
+	protected String debugString() {
+		return Arrays.toString(queue.toArray());
+	}
+}
