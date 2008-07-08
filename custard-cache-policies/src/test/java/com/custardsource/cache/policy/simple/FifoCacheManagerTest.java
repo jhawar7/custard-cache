8e35617746f7a9e66a479b2fb50f5cfc8098d30c
funkwit <funkwit@2972ba69-cd50-0410-870e-89e834fb94c0>
2008-07-08 12:26:42 +0000

    Track misses too

--- custard-cache-policies/src/test/java/com/custardsource/cache/policy/simple/FifoCacheManagerTest.java
+++ custard-cache-policies/src/test/java/com/custardsource/cache/policy/simple/FifoCacheManagerTest.java
@@ -10,10 +10,11 @@ public class FifoCacheManagerTest extends BasicCacheStateTest {
 				new FifoConfiguration(3));
 		EvictionListener listener = new EvictionListener();
 		manager.addListener(listener);
-		assertStateAfterVisit(manager, 'A', "[A]");
-		assertStateAfterVisit(manager, 'B', "[A, B]");
-		assertStateAfterVisit(manager, 'C', "[A, B, C]");
-		assertStateAfterVisit(manager, 'D', "[B, C, D]", listener, 1);
-		assertStateAfterVisit(manager, 'C', "[B, C, D]");
+		assertStateAfterVisit(manager, listener, 'A', "[A]", 1, 0);
+		assertStateAfterVisit(manager, listener, 'A', "[A]", 0, 0);
+		assertStateAfterVisit(manager, listener, 'B', "[A, B]", 1, 0);
+		assertStateAfterVisit(manager, listener, 'C', "[A, B, C]", 1, 0);
+		assertStateAfterVisit(manager, listener, 'D', "[B, C, D]", 1, 1);
+		assertStateAfterVisit(manager, listener, 'C', "[B, C, D]", 0, 0);
 	}
 }
