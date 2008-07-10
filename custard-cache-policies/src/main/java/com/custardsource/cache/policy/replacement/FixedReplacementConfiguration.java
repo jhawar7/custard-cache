725f3bb9584728498cdfecd15c4bfe8fd502dd96
funkwit <funkwit@2972ba69-cd50-0410-870e-89e834fb94c0>
2008-07-10 10:58:18 +0000

    Test case for illegal FRC configurations

--- custard-cache-policies/src/main/java/com/custardsource/cache/policy/replacement/FixedReplacementConfiguration.java
+++ custard-cache-policies/src/main/java/com/custardsource/cache/policy/replacement/FixedReplacementConfiguration.java
@@ -11,11 +11,15 @@ public class FixedReplacementConfiguration extends BasicConfiguration {
 
 	public FixedReplacementConfiguration(int maxNodes, int t1Size) {
 		super(maxNodes);
+        if (t1Size <= 0 || t1Size >= maxNodes) {
+            throw new IllegalArgumentException("t1 size of " + t1Size + " in a " + maxNodes
+                    + "-sized cache would result in a simple LRU cache");
+        }
 		this.t1Size = t1Size;
 	}
 
 	public FixedReplacementConfiguration(int maxNodes, double t1Percent) {
-		this(maxNodes, (int) t1Percent * maxNodes);
+		this(maxNodes, (int) (t1Percent * maxNodes));
 	}
 
 	public int getT1Size() {
